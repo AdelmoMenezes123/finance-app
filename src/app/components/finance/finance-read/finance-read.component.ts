1e403070c20d1cf4e8dfc1fdaddc65a4d58b16f3
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-12-19 22:12:52 -0300

    cria tela de listagem, layout de card e informacoes

--- src/app/components/finance/finance-read/finance-read.component.ts
+++ src/app/components/finance/finance-read/finance-read.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinanceReadComponent implements OnInit {
 
+  longText = `fermento em po`;
+
   constructor() { }
 
   ngOnInit(): void {
