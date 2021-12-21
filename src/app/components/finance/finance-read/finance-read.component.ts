02655d7eb5f3ce54ea0789562c27f8658cb24501
Adelmo Menezes <adelmo.menezes2010@gmail.com>
2021-12-20 22:19:39 -0300

    card dinamico

--- src/app/components/finance/finance-read/finance-read.component.ts
+++ src/app/components/finance/finance-read/finance-read.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FinanceReadComponent implements OnInit {
 
-  longText = `fermento em po`;
+  dados = [{
+    titulo: "Chocolate",
+    status: "Investimento",
+    data: new Date(),
+    quantidade: 10,
+    valor_unitario: 20.30,
+    user: "23d896hgd89000300"
+  },
+  {
+    titulo: "Bolo pote",
+    status: "Lucro",
+    data: new Date(),
+    quantidade: 10,
+    valor_unitario: 12.00,
+    user: "23d896hgd89000300"
+  },
+  {
+    titulo: "Leite moca",
+    status: "Investimento",
+    data: new Date(),
+    quantidade: 10,
+    valor_unitario: 20.30,
+    user: "23d896hgd89000300"
+  }];
 
   constructor() { }
 
